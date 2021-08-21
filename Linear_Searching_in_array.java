273d8e34d91e3fbf2cdf813d015bfc6a89d7c255
Ankur Rohilla <59440628+ankur-lakhmara@users.noreply.github.com>
2021-08-21 17:55:59 +0530

    Create Linear_Searching_in_array.java

---
+++ Linear_Searching_in_array.java
@@ -0,0 +1,23 @@
+package Coding_Ninja_Array;
+
+import java.util.Scanner;
+
+public class Linear_Searching_in_array {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of array : ");
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the number that you want to find : ");
+        int b = sc.nextInt();
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]==b){
+                System.out.println("The element is present at "+i+"th position");
+            }
+        }
+    }
+}
